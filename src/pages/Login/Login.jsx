4500e1235f92e502cdc09324f4169c14219f9bb0
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-03-02 09:06:10 -0500

    boiler login/signup pages/form

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Login.module.css';
+
+const Login = (props) => {
+    return (
+        <main>
+            <h1>Login</h1>
+        </main>
+    );
+};
+
+export default Login;
