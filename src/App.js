6814f3ea1738e46d3740dc92f18ac3806054422c
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-03-02 09:06:37 -0500

    added events page/ app.js updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route, Router} from 'react-router-dom';
+ 
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home/Home';
+import Events from './pages/Events/Events';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-outer-container">
+      <Navbar />
+      <div className="App-inner-container">
+        <Switch>
+          <Route exact path="/" render={props => 
+            <Home />
+           }/>
+          <Route exact path="/events" render={props =>
+          <Events />
+        }/>
+          <Route exact path="/login" render={props => 
+            <Login />
+           }/>
+          <Route exact path="/signup" render={props =>
+          <Signup />
+        }/>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+// join pages to database..
+//add oauth 
+//drop down navbar - connect to event api ??
+
---
+++ src/pages/Events/Events.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Events.module.css';
+
+const Events = (props) => {
+    return (
+        <main>
+            <h1>Events</h1>
+        </main>
+    )
+}
+
+export default Events;
