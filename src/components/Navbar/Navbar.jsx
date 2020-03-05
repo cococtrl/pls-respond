8482c6b4428bd0c88d739d4774d9c132c7cf8cbe
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-03-05 07:51:17 -0500

    navbar refactor

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+
+import userService from '../../utils/userService';
 
 import styles from './Navbar.module.css';
 
 const Navbar = (props) => {
+    const conditionalUI = userService.getUser()
+    ? <>
+        <li>
+            <Link to="/">Public Events</Link>
+        </li>
+        <li>
+            <Link to="/login">My Events</Link>
+        </li>
+        <li>
+            <Link to="" onClick={props.handleLogout}>Logout</Link>
+        </li>
+      </>
+    :
+      <>
+        <li>
+           <Link to="/">Events</Link>  
+        </li>
+        <li>
+            <Link to="/login">Login</Link>
+        </li>
+        <li>
+            <Link to="/signup">Signup</Link>
+        </li>
+      </>
     return (
         <nav className={styles.navbar}>
             <Link to="/">
                 <h1>Pls Respond</h1>
             </Link>
             <ul>
-                <li>
-                    <Link to="/">Events</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/signup">Signup</Link>
-                </li>
+                { conditionalUI }
             </ul>
         </nav>
     )
